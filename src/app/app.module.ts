2d8d7d5f36aa1ddc0201d563336bfb3ffe1471e1
DESKTOP-VBTHFHE\1 <kredinec.v@gmail.com>
2022-12-24 16:02:41 +0200

    Добавили задачі(без фільтрації по категорії)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import { CategoriesComponent } from './views/categories/categories.component';
+import { TasksComponent } from './views/tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CategoriesComponent,
+    TasksComponent,
 
   ],
   imports: [
