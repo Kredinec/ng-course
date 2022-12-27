9d25786e8114ca2e4eed2a115f5ce37f73ff58ef
DESKTOP-VBTHFHE\1 <kredinec.v@gmail.com>
2022-12-27 13:58:41 +0200

    Добавили об,єкти Subject і підписників для відображення задач при виборі категорії(всі задачі поки що не показуються)

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -2,11 +2,13 @@ import {Injectable} from '@angular/core';
 import {Category} from "../model/Category";
 import {TestData} from "../data/TestData";
 import {Task} from "../model/Task";
+import {Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
+  tasksSubject = new Subject<Task[]>();
 
   constructor() {
 
@@ -16,12 +18,12 @@ export class DataHandlerService {
     return TestData.categories;
   }
 
-  getTasks(): Task[] {
-    return TestData.tasks;
+  fillTasks(){
+    this.tasksSubject.next(TestData.tasks);
   }
 
-  getTasksByCategory(category: Category): Task[] {
+  fillTasksByCategory(category: Category) {
     const tasks = TestData.tasks.filter(task => task.category === category);
-    return tasks;
+    this.tasksSubject.next(tasks);
   }
 }
